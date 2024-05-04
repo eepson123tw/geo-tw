72b14caa7630110a48af6f67cf633e94af4a4814
allen <eepson123@gmail.com>
2024-05-04 15:11:52 +0800

    feat(geo-tw): add label

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import GEO from "./geo";
-function App() {
+
+const GEOTW = () => {
   return (
     <>
-      <h1>GEO TW</h1>
+      <h2>Visx</h2>
+      <p>並非最新圖資</p>
       <GEO width={500} height={500}></GEO>
     </>
+  );
+};
+
+function App() {
+  const [optionState, setOptionState] = useState("");
+
+  useEffect(() => {
+    if (optionState === "") {
+      setOptionState("visx-tw");
+    }
+  }, [optionState]);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <h1>GEO TW</h1>
+      <div className="btn-list">
+        <button onClick={() => setOptionState("default")}>Default</button>
+        <button onClick={() => setOptionState("visx-tw")}>Visx</button>
+      </div>
+      {optionState === "visx-tw" ? <GEOTW></GEOTW> : null}
+    </div>
   );
 }
 
--- src/geo.tsx
+++ src/geo.tsx
@@ -1,7 +1,8 @@
-import { scaleQuantize } from "@visx/scale";
+import { scaleQuantize, scaleOrdinal } from "@visx/scale";
 import { Mercator, Graticule } from "@visx/geo";
 import { Zoom } from "@visx/zoom";
 import { geoCentroid } from "@visx/vendor/d3-geo";
+import { LegendOrdinal, LegendItem, LegendLabel } from "@visx/legend";
 import * as topojson from "topojson-client";
 import topology from "@assets/taiwan-topo.json";
 import React from "react";
@@ -45,91 +46,169 @@ const color = scaleQuantize({
   ],
 });
 
+const GEOLegend = (data: { data: typeof world }) => {
+  const glyphSize = 20;
+
+  const labelAry = new Set(
+    data.data.features
+      .map((f) => "城市複雜度 => " + f.geometry.coordinates.length)
+      .sort()
+  );
+
+  const ordinalColorScale = scaleOrdinal({
+    domain: Array.from(labelAry),
+    range: [
+      "#ffb01d",
+      "#ffa020",
+      "#ff9221",
+      "#ff8424",
+      "#ff7425",
+      "#fc5e2f",
+      "#f94b3a",
+      "#f63a48",
+    ],
+  });
+
+  return (
+    <div
+      style={{
+        marginRight: "20px",
+      }}
+    >
+      <LegendOrdinal
+        scale={ordinalColorScale}
+        // labelFormat={(label) => `${label.toUpperCase()}`}
+      >
+        {(labels) => (
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            {labels.map((label, i) => (
+              <LegendItem
+                key={`legend-quantile-${i}`}
+                margin="5px 0"
+                onClick={() => {
+                  console.log(`clicked: ${JSON.stringify(label)}`);
+                }}
+              >
+                <svg width={glyphSize} height={glyphSize}>
+                  <circle
+                    cx={glyphSize / 2}
+                    cy={glyphSize / 2}
+                    r={glyphSize / 4}
+                    stroke={label.value}
+                    strokeWidth="3px"
+                    width={glyphSize}
+                    height={glyphSize}
+                    fill="none"
+                  />
+                </svg>
+                <LegendLabel align="left" margin="0 0 0 4px">
+                  {label.text}
+                </LegendLabel>
+              </LegendItem>
+            ))}
+          </div>
+        )}
+      </LegendOrdinal>
+    </div>
+  );
+};
+
 export default function GEO({ width, height }: GeoMercatorProps) {
   const centerX = width / 2;
   const centerY = height / 2;
   const scale = 4600;
 
   return width < 10 ? null : (
-    <Zoom<SVGSVGElement>
-      width={width}
-      height={height}
-      scaleXMin={1 / 2}
-      scaleXMax={4}
-      scaleYMin={1 / 2}
-      scaleYMax={4}
+    <div
+      style={{
+        position: "relative",
+        display: "flex",
+        flexDirection: "row-reverse",
+      }}
     >
-      {(zoom) => (
-        <svg
-          width={width}
-          height={height}
-          style={{
-            cursor: zoom.isDragging ? "grabbing" : "grab",
-            touchAction: "none",
-            border: "1px solid #ccc",
-          }}
-          ref={zoom.containerRef}
-        >
-          <>
-            <rect
-              width={width}
-              height={height}
-              fill={background}
-              rx={14}
-              onTouchStart={zoom.dragStart}
-              onTouchMove={zoom.dragMove}
-              onTouchEnd={zoom.dragEnd}
-              onMouseDown={zoom.dragStart}
-              onMouseMove={zoom.dragMove}
-              onMouseUp={zoom.dragEnd}
-              onMouseLeave={() => {
-                if (zoom.isDragging) zoom.dragEnd();
-              }}
-            />
-            <Mercator<FeatureShape>
-              data={world.features}
-              scale={scale}
-              translate={[centerX - 9700, centerY + 1970]}
-              centroid={([x, y]) => (
-                <circle cx={x} cy={y} r={20} stroke="black" />
-              )}
-            >
-              {(mercator) => (
-                <g transform={zoom.toString()}>
-                  <Graticule
-                    graticule={(g) => mercator.path(g) || ""}
-                    stroke="rgba(33,33,33,0)"
-                  />
-                  {mercator.features.map(({ feature, path, projection }, i) => (
-                    <React.Fragment
-                      key={`map-feature-${i}-${feature.geometry.coordinates.length}`}
-                    >
-                      <path
-                        d={path || ""}
-                        fill={color(feature.geometry.coordinates.length)}
-                        stroke={background}
-                        strokeWidth={0.1}
-                        onClick={() => {
-                          alert(JSON.stringify(feature));
-                        }}
-                      />
-                      <text
-                        transform={`translate(${projection(
-                          geoCentroid(feature)
-                        )})`}
-                        fontSize={3}
-                        textAnchor="middle"
-                      >
-                        {feature.properties.name}
-                      </text>
-                    </React.Fragment>
-                  ))}
-                </g>
-              )}
-            </Mercator>
-          </>
-        </svg>
-      )}
-    </Zoom>
+      <Zoom<SVGSVGElement>
+        width={width}
+        height={height}
+        scaleXMin={1 / 2}
+        scaleXMax={4}
+        scaleYMin={1 / 2}
+        scaleYMax={4}
+      >
+        {(zoom) => (
+          <svg
+            width={width}
+            height={height}
+            style={{
+              cursor: zoom.isDragging ? "grabbing" : "grab",
+              touchAction: "none",
+              border: "1px solid #ccc",
+            }}
+            ref={zoom.containerRef}
+          >
+            <>
+              <rect
+                width={width}
+                height={height}
+                fill={background}
+                rx={14}
+                onTouchStart={zoom.dragStart}
+                onTouchMove={zoom.dragMove}
+                onTouchEnd={zoom.dragEnd}
+                onMouseDown={zoom.dragStart}
+                onMouseMove={zoom.dragMove}
+                onMouseUp={zoom.dragEnd}
+                onMouseLeave={() => {
+                  if (zoom.isDragging) zoom.dragEnd();
+                }}
+              />
+              <Mercator<FeatureShape>
+                data={world.features}
+                scale={scale}
+                translate={[centerX - 9700, centerY + 1970]}
+                centroid={([x, y]) => (
+                  <circle cx={x} cy={y} r={20} stroke="black" />
+                )}
+              >
+                {(mercator) => (
+                  <g transform={zoom.toString()}>
+                    <Graticule
+                      graticule={(g) => mercator.path(g) || ""}
+                      stroke="rgba(33,33,33,0)"
+                    />
+                    {mercator.features.map(
+                      ({ feature, path, projection }, i) => (
+                        <React.Fragment
+                          key={`map-feature-${i}-${feature.geometry.coordinates.length}`}
+                        >
+                          <path
+                            d={path || ""}
+                            fill={color(feature.geometry.coordinates.length)}
+                            stroke={background}
+                            strokeWidth={0.1}
+                            onClick={() => {
+                              alert(JSON.stringify(feature));
+                            }}
+                          />
+                          <text
+                            transform={`translate(${projection(
+                              geoCentroid(feature)
+                            )})`}
+                            fontSize={3}
+                            textAnchor="middle"
+                          >
+                            {feature.properties.name}
+                          </text>
+                        </React.Fragment>
+                      )
+                    )}
+                  </g>
+                )}
+              </Mercator>
+            </>
+          </svg>
+        )}
+      </Zoom>
+      <GEOLegend data={world}></GEOLegend>
+    </div>
   );
 }
